580d84ad1ffa18ce30df682933516ce3532c025d
mkm <mkm@mkm.nabto.com>
2020-03-25 12:49:04 +0100

    fixed log

--- include/nn/log.h
+++ include/nn/log.h
@@ -20,15 +20,17 @@ struct nn_log {
     nn_log_print logFunction;
 };
 
+void nn_log_adapter(struct nn_log* logger, enum nn_log_severity severity, const char* module, const char* file, int line, const char* fmt, ...);
+
 #define VA_ARGS(...) , ##__VA_ARGS__
 
-#define NN_LOG_ERROR(logger, module, fmt, ...) do { (logger)->logFunction(NN_LOG_SEVERITY_ERROR, module, __FILE__, __LINE__, fmt VA_ARGS(__VA_ARGS__)); } while (0)
+#define NN_LOG_ERROR(logger, module, fmt, ...) do { nn_log_adapter(logger, NN_LOG_SEVERITY_ERROR, module, __FILE__, __LINE__, fmt VA_ARGS(__VA_ARGS__)); } while (0)
 
-#define NN_LOG_WARN(logger, module, fmt, ...) do { logger->logFunction(NN_LOG_SEVERITY_WARN, module, __FILE__, __LINE__, fmt VA_ARGS(__VA_ARGS__)); } while (0)
+#define NN_LOG_WARN(logger, module, fmt, ...) do { nn_log_adapter(logger, NN_LOG_SEVERITY_WARN, module, __FILE__, __LINE__, fmt VA_ARGS(__VA_ARGS__)); } while (0)
 
-#define NN_LOG_INFO(logger, module, fmt, ...) do { logger->logFunction(NN_LOG_SEVERITY_INFO, module, __FILE__, __LINE__, fmt VA_ARGS(__VA_ARGS__)); } while (0)
+#define NN_LOG_INFO(logger, module, fmt, ...) do { nn_log_adapter(logger, NN_LOG_SEVERITY_INFO, module, __FILE__, __LINE__, fmt VA_ARGS(__VA_ARGS__)); } while (0)
 
-#define NN_LOG_TRACE(logger, module, fmt, ...) do { logger->logFunction(NN_LOG_SEVERITY_TRACE, module, __FILE__, __LINE__, fmt VA_ARGS(__VA_ARGS__)); } while (0)
+#define NN_LOG_TRACE(logger, module, fmt, ...) do { nn_log_adapter(logger, NN_LOG_SEVERITY_TRACE, module, __FILE__, __LINE__, fmt VA_ARGS(__VA_ARGS__)); } while (0)
 
 #ifdef __cplusplus
 } //extern "C"
